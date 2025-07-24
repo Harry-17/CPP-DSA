667d0ae6db7360ec13f70e05e1b0219403daf7b4
Harsh <hammadhur17@gmail.com>
2025-07-24 20:08:49 +0530

    Added my professional summary

--- hello.cpp
+++ hello.cpp
@@ -2,6 +2,6 @@
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
+    cout << "Final-year B.Tech Computer Science and Engineering student with a strong foundation in Web Development, Machine Learning, and Artificial Intelligence. Proficient in modern Web technologies (HTML, CSS, JavaScript, React.js, Next.js, TailwindCSS) and experienced in developing ML models using Python, TensorFlow, and Scikit-learn. Passionate about building intelligent, user-centric applications. Quick learner with strong problem-solving skills and a keen interest in emerging technologies." << endl;
     return 0;
 }
